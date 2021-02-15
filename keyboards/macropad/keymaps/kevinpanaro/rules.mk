e2b9987f671034ed9cac4aad9f46eee81c53cf3b
Kevin J Panaro <panaro.kevin@gmail.com>
2021-02-14 17:10:58 -0700

    add potential led support

--- keyboards/macropad/keymaps/kevinpanaro/rules.mk
+++ keyboards/macropad/keymaps/kevinpanaro/rules.mk
@@ -4,3 +4,4 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 TAP_DANCE_ENABLE = yes      # enable tap dance for numpad 0/1
 COMBO_ENABLE = yes          # enable combos
 RAW_ENABLE = yes            # enable hid for bidirectional communication
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
