48ce12b84c7f073dec03c7b8b19540d2612e4c89
Kevin Panaro <panaro.kevin@gmail.com>
2021-12-06 11:22:10 -0700

    macropad default initial commit

--- keyboards/macropad/macropad.h
+++ keyboards/macropad/macropad.h
@@ -1,10 +1,24 @@
-// Copyright 2021 Kevin Panaro (@Kevin Panaro)
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright 2021 Kevin Panaro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
 #include "quantum.h"
 
+#define ___ KC_NO
 /* This is a shortcut to help you visually see your layout.
  *
  * The first section contains all of the arguments representing the physical
@@ -14,9 +28,13 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
+    k00,                 \
+         k11, k12, k13,  \
+         k21, k22, k23,  \
+         k31, k32, k33   \
 ) { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
+    { k00, ___, ___, ___, }, \
+    { ___, k11, k12, k13, }, \
+    { ___, k21, k22, k23, }, \
+    { ___, k31, k32, k33, }  \
 }
