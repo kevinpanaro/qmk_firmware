5399fc0296d26aa4b3096eec2297cb2d5955324d
Kevin Panaro <panaro.kevin@gmail.com>
2021-12-21 20:02:38 -0700

    keymap rework

--- keyboards/sofle/keymaps/kevinpanaro/keymap.c
+++ keyboards/sofle/keymaps/kevinpanaro/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-               KC_LOWER ,KC_LALT, KC_LGUI, KC_LCTRL , KC_SPC,      KC_ENT,  KC_RCTRL, KC_RGUI, KC_RALT, KC_RAISE
+               KC_LOWER ,KC_LALT, KC_LGUI, KC_LCTRL , KC_SPC,      KC_ENT,  KC_RAISE, KC_RGUI, KC_RALT, KC_RCTRL
 ),
 /*
  * VALORANT
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB  ,   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_CAPS ,  KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LCTRL,  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_B, KC_MUTE,    KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                KC_LOWER,KC_LALT, KC_G,   KC_LCTRL,    KC_SPC,           KC_ENT,  KC_RCTRL, KC_RGUI, KC_RALT, KC_RAISE
+                KC_LOWER,KC_LALT, KC_G,   KC_LCTRL,    KC_SPC,           KC_ENT,  KC_RAISE, KC_RGUI, KC_RALT, KC_RCTRL
 ),
 /*
  * APEX
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_M  ,  KC_TAB ,   KC_Q,    KC_W,    KC_E,    KC_R,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_CAPS ,   KC_G ,   KC_A,    KC_S,    KC_D,    KC_F,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LCTRL,  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,    KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                KC_LOWER, KC_LALT, XXXXXXX, XXXXXXX, KC_SPC,      KC_B,  KC_RCTRL, KC_RGUI, KC_RALT, KC_RAISE
+                KC_LOWER, KC_LALT, XXXXXXX, XXXXXXX, KC_SPC,      KC_B,  KC_RAISE, KC_RGUI, KC_RALT, KC_RCTRL
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
